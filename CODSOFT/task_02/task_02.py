44e832848cae5bd9283187fe9c25d01588adf0c0
jbjzeehad <ilamta.97@gmail.com>
2023-08-17 19:17:58 +0600

    ui cal

--- CODSOFT/task_02/task_02.py
+++ CODSOFT/task_02/task_02.py
@@ -23,13 +23,30 @@ def div_cal(num1, num2):
 
 
 cal_sc = Tk()
-cal_sc.geometry("500x700")
+cal_sc.geometry("385x400")
 cal_sc.resizable(False, False)
 cal_sc.title('Calculator')
-# cal_sc.iconbitmap(r')
+cal_sc.iconbitmap(r'F:\codsoft_internship\CODSOFT\task_02\cal_bg.ico')
 
-cal_sc_output = Text(cal_sc, width=20, height=3, font=('Consolas', 20))
-cal_sc_output.grid(columnspan=5)
+cal_sc_output = Text(cal_sc, width=24, height=2, font=('Consolas', 20))
+cal_sc_output.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
+
+Button(cal_sc, text='7').grid(row=1, column=0)
+Button(cal_sc, text='8').grid(row=1, column=1)
+Button(cal_sc, text='9').grid(row=1, column=2)
+Button(cal_sc, text='/').grid(row=1, column=3)
+Button(cal_sc, text='4').grid(row=2, column=0)
+Button(cal_sc, text='5').grid(row=2, column=1)
+Button(cal_sc, text='6').grid(row=2, column=2)
+Button(cal_sc, text='X').grid(row=2, column=3)
+Button(cal_sc, text='1').grid(row=3, column=0)
+Button(cal_sc, text='2').grid(row=3, column=1)
+Button(cal_sc, text='3').grid(row=3, column=2)
+Button(cal_sc, text='-').grid(row=3, column=3)
+Button(cal_sc, text='C').grid(row=4, column=0)
+Button(cal_sc, text='0').grid(row=4, column=1)
+Button(cal_sc, text='=').grid(row=4, column=2)
+Button(cal_sc, text='+').grid(row=4, column=3)
 
 cal_sc.mainloop()
