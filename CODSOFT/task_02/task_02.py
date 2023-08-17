ff0797f91a2dd509084bf84a82b199b8885f3c8a
jbjzeehad <ilamta.97@gmail.com>
2023-08-17 17:11:30 +0600

    calcul

--- CODSOFT/task_02/task_02.py
+++ CODSOFT/task_02/task_02.py
@@ -1,6 +1,27 @@
 
 from tkinter import *
 
+
+def add_cal(num1, num2):
+    add_tot = num1 + num2
+    return add_tot
+
+
+def sub_cal(num1, num2):
+    sub_tot = num1 - num2
+    return sub_tot
+
+
+def mul_cal(num1, num2):
+    mul_tot = num1 * num2
+    return mul_tot
+
+
+def div_cal(num1, num2):
+    div_tot = num1 / num2
+    return div_tot
+
+
 cal_sc = Tk()
 cal_sc.geometry("500x700")
 cal_sc.resizable(False, False)
