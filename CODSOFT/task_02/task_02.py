191368b590818757888295852c65f0a41e852901
jbjzeehad <ilamta.97@gmail.com>
2023-08-17 17:02:26 +0600

    ui

--- CODSOFT/task_02/task_02.py
+++ CODSOFT/task_02/task_02.py
@@ -3,7 +3,12 @@ from tkinter import *
 
 cal_sc = Tk()
 cal_sc.geometry("500x700")
-cal_sc.title("calculator")
-# cal_sc.iconbitmap()
+cal_sc.resizable(False, False)
+cal_sc.title('Calculator')
+# cal_sc.iconbitmap(r')
+
+cal_sc_output = Text(cal_sc, width=20, height=3, font=('Consolas', 20))
+cal_sc_output.grid(columnspan=5)
+
 
 cal_sc.mainloop()
