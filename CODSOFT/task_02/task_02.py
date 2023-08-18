fbd6f98425996df3918b02104c58858cdda69e36
jbjzeehad <ilamta.97@gmail.com>
2023-08-18 17:58:54 +0600

    ui done

--- CODSOFT/task_02/task_02.py
+++ CODSOFT/task_02/task_02.py
@@ -23,30 +23,48 @@ def div_cal(num1, num2):
 
 
 cal_sc = Tk()
-cal_sc.geometry("385x400")
+cal_sc.geometry("440x440")
 cal_sc.resizable(False, False)
 cal_sc.title('Calculator')
 cal_sc.iconbitmap(r'F:\codsoft_internship\CODSOFT\task_02\cal_bg.ico')
+cal_sc.config(bg='orange')
 
-cal_sc_output = Text(cal_sc, width=24, height=2, font=('Consolas', 20))
-cal_sc_output.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+cal_sc_output = Text(cal_sc, background='black', fg='white', width=29, height=3,
+                     borderwidth=1, font=('Consolas', 20))
+cal_sc_output.grid(row=0, column=0, columnspan=4, padx=1, pady=2)
 
 
-Button(cal_sc, text='7').grid(row=1, column=0)
-Button(cal_sc, text='8').grid(row=1, column=1)
-Button(cal_sc, text='9').grid(row=1, column=2)
-Button(cal_sc, text='/').grid(row=1, column=3)
-Button(cal_sc, text='4').grid(row=2, column=0)
-Button(cal_sc, text='5').grid(row=2, column=1)
-Button(cal_sc, text='6').grid(row=2, column=2)
-Button(cal_sc, text='X').grid(row=2, column=3)
-Button(cal_sc, text='1').grid(row=3, column=0)
-Button(cal_sc, text='2').grid(row=3, column=1)
-Button(cal_sc, text='3').grid(row=3, column=2)
-Button(cal_sc, text='-').grid(row=3, column=3)
-Button(cal_sc, text='C').grid(row=4, column=0)
-Button(cal_sc, text='0').grid(row=4, column=1)
-Button(cal_sc, text='=').grid(row=4, column=2)
-Button(cal_sc, text='+').grid(row=4, column=3)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='7').grid(row=1, column=0)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='8').grid(row=1, column=1, pady=1)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='9').grid(row=1, column=2)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='/').grid(row=1, column=3)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='4').grid(row=2, column=0)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='5').grid(row=2, column=1, pady=1)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='6').grid(row=2, column=2)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='X').grid(row=2, column=3)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='1').grid(row=3, column=0)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='2').grid(row=3, column=1, pady=1)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='3').grid(row=3, column=2)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='-').grid(row=3, column=3)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='C').grid(row=4, column=0)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='0').grid(row=4, column=1, pady=1)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='=').grid(row=4, column=2)
+Button(cal_sc, background='black', fg='white', width=10, height=3, borderwidth=1,
+       text='+').grid(row=4, column=3)
 
 cal_sc.mainloop()
