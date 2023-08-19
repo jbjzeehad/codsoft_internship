1fbae39c8e2e4b5b406edb4b1e644c6ab691a6ef
jbjzeehad <ilamta.97@gmail.com>
2023-08-19 17:47:37 +0600

    start window done

--- CODSOFT/task_01/task_01.py
+++ CODSOFT/task_01/task_01.py
@@ -56,51 +56,66 @@ rps_wind.resizable(False, False)
 rps_wind.title("RPS Game")
 rps_wind.iconbitmap(r"F:\codsoft_internship\CODSOFT\task_01\rps_logo.ico")
 
-
+game_logo = PhotoImage(
+    file=r'F:\codsoft_internship\CODSOFT\task_01\rps_logo.png')
+label = Label(rps_wind, image=game_logo).pack()
 Button(rps_wind, text="strt window",
-       command=lambda: [rps_wind.destroy(), strt_wind()]).grid(row=4, column=0)
+       command=lambda: [rps_wind.destroy(), game_wind()]).pack(side=BOTTOM)
 
 
-def strt_wind():
+def instr_wind():
+    print("ok")
     rps_wind = Tk()
     rps_wind.geometry("500x550")
     rps_wind.resizable(False, False)
     rps_wind.title("RPS Game")
     rps_wind.iconbitmap(
         r"F:\codsoft_internship\CODSOFT\task_01\rps_logo.ico")
-    Button(rps_wind, command=exit_wind())
+    Button(rps_wind, text="START GAME", command=lambda: [
+           rps_wind.destroy(), game_wind()])
+    rps_wind.mainloop()
+
+
+def game_wind():
+    rps_wind = Tk()
+    rps_wind.geometry("500x550")
+    rps_wind.resizable(False, False)
+    rps_wind.title("RPS Game")
+    rps_wind.iconbitmap(
+        r"F:\codsoft_internship\CODSOFT\task_01\rps_logo.ico")
+    Label(rps_wind, text='pc pic', font=('consolas', 20)).grid(row=0, column=1)
+    Label(rps_wind, text='pcr', font=('consolas', 20)).grid(row=1, column=0)
+    Label(rps_wind, text='pcw', font=('consolas', 20)).grid(row=1, column=1)
+    Label(rps_wind, text='pcp', font=('consolas', 20)).grid(row=1, column=2)
+    Label(rps_wind, text='up', font=('consolas', 20)).grid(row=2, column=0)
+    Label(rps_wind, text='uw', font=('consolas', 20)).grid(row=2, column=1)
+    Label(rps_wind, text='ur', font=('consolas', 20)).grid(row=2, column=2)
+    Button(rps_wind, text="TRY AGAIN", command=lambda: [
+           rps_wind.destroy(), exit_wind()])
+
+    image1 = PhotoImage(
+        file=r"F:\codsoft_internship\CODSOFT\task_01\rock_img.png")
+    rock_img = image1.subsample(4, 4)
+    Button(rps_wind, text='ROCK', activebackground='green', bd=2, fg='teal', font=('Cooper Black', 15),
+           image=rock_img, compound=TOP).grid(row=3, column=0, padx=10)  # button padding from window, side from window
+
+    image2 = PhotoImage(
+        file=r"F:\codsoft_internship\CODSOFT\task_01\paper_img.png")
+    paper_img = image2.subsample(4, 4)
+    Button(rps_wind, bd=2, text='PAPER', activebackground='green', fg='orange', font=('Cooper Black', 15),
+           image=paper_img, compound=TOP).grid(row=3, column=1, padx=10)
+
+    image3 = PhotoImage(
+        file=r"F:\codsoft_internship\CODSOFT\task_01\scissor_img.png")
+    scissor_img = image3.subsample(4, 4)
+    Button(rps_wind, bd=2, fg='indigo', activebackground='green', text='SCISSOR', font=('Cooper Black', 15),
+           image=scissor_img, compound=TOP).grid(row=3, column=2, padx=10)
+
     rps_wind.mainloop()
 
 
 def exit_wind():
-    return 0
-
-
-Label(rps_wind, text='pc pic', font=('consolas', 20)).grid(row=0, column=1)
-Label(rps_wind, text='pcr', font=('consolas', 20)).grid(row=1, column=0)
-Label(rps_wind, text='pcw', font=('consolas', 20)).grid(row=1, column=1)
-Label(rps_wind, text='pcp', font=('consolas', 20)).grid(row=1, column=2)
-Label(rps_wind, text='up', font=('consolas', 20)).grid(row=2, column=0)
-Label(rps_wind, text='uw', font=('consolas', 20)).grid(row=2, column=1)
-Label(rps_wind, text='ur', font=('consolas', 20)).grid(row=2, column=2)
-
-image1 = PhotoImage(file=r"F:\codsoft_internship\CODSOFT\task_01\rock_img.png")
-rock_img = image1.subsample(4, 4)
-Button(rps_wind, text='ROCK', activebackground='green', bd=2, fg='teal', font=('Cooper Black', 15),
-       image=rock_img, compound=TOP).grid(row=3, column=0, padx=10)  # button padding from window, side from window
-
-image2 = PhotoImage(
-    file=r"F:\codsoft_internship\CODSOFT\task_01\paper_img.png")
-paper_img = image2.subsample(4, 4)
-Button(rps_wind, bd=2, text='PAPER', activebackground='green', fg='orange', font=('Cooper Black', 15),
-       image=paper_img, compound=TOP).grid(row=3, column=1, padx=10)
-
-
-image3 = PhotoImage(
-    file=r"F:\codsoft_internship\CODSOFT\task_01\scissor_img.png")
-scissor_img = image3.subsample(4, 4)
-Button(rps_wind, bd=2, fg='indigo', activebackground='green', text='SCISSOR', font=('Cooper Black', 15),
-       image=scissor_img, compound=TOP).grid(row=3, column=2, padx=10)
+    rps_wind.destroy()
 
 
 rps_wind.mainloop()
