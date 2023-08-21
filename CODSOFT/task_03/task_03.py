a8fcf170eaf6ce9670a96dc1d80e47b1faa1b6ad
jbjzeehad <ilamta.97@gmail.com>
2023-08-21 20:26:09 +0600

    finally done

--- CODSOFT/task_03/task_03.py
+++ CODSOFT/task_03/task_03.py
@@ -13,6 +13,7 @@ rpg_wid.geometry('366x370')
 rpg_wid.resizable(False, False)
 rpg_wid.title('Password Generator')
 rpg_wid.iconbitmap(r'F:\codsoft_internship\CODSOFT\task_03\rpg.ico')
+rpg_wid.config(bg='white')
 
 
 def weak():
@@ -21,8 +22,10 @@ def weak():
     weak_pass = 'abcdefghijklmnopqrstuvwxyz'
     for i in range(0, length):
         final_pass = final_pass + random.choice(weak_pass)
-    Label(rpg_wid, text=final_pass, font=('console', 13)).grid(
-        row=1, column=0, columnspan=3, pady=15)
+    password = StringVar()
+    password.set(final_pass)
+    Entry(
+        rpg_wid, textvariable=password, font=('console', 13), justify=CENTER, width=39, disabledbackground='white', borderwidth=0).grid(row=1, column=0, columnspan=3, pady=12)
 
 
 def medium():
@@ -31,8 +34,10 @@ def medium():
     medium_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     for i in range(0, length):
         final_pass = final_pass + random.choice(medium_pass)
-    Label(rpg_wid, text=final_pass, font=('console', 13)).grid(
-        row=1, column=0, columnspan=3, pady=15)
+    password = StringVar()
+    password.set(final_pass)
+    Entry(
+        rpg_wid, textvariable=password, font=('console', 13), justify=CENTER, width=39, disabledbackground='white', borderwidth=0).grid(row=1, column=0, columnspan=3, pady=12)
 
 
 def strong():
@@ -41,20 +46,23 @@ def strong():
     strong_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
     for i in range(0, length):
         final_pass = final_pass + random.choice(strong_pass)
-    Label(rpg_wid, text=final_pass, font=('console', 13)).grid(
-        row=1, column=0, columnspan=3, pady=15)
+    password = StringVar()
+    password.set(final_pass)
+    Entry(
+        rpg_wid, textvariable=password, font=('console', 13), justify=CENTER, width=39, disabledbackground='white', borderwidth=0).grid(row=1, column=0, columnspan=3, pady=12)
 
 
 length_pass = StringVar()
 length_pass_entry = Entry(
-    rpg_wid, textvariable=length_pass, font=('verdana', 20), justify=CENTER, borderwidth=0).grid(row=2, column=0, columnspan=3, pady=21)
+    rpg_wid, textvariable=length_pass, font=('verdana', 20), justify=CENTER, borderwidth=1).grid(row=2, column=0, columnspan=3, pady=12)
 
 top_img = PhotoImage(file="F:\codsoft_internship\CODSOFT\\task_03\\rpg_bg.png")
 top_bg_img = top_img.subsample(3, 3)
 Label(rpg_wid, image=top_bg_img).grid(
     row=0, column=0, columnspan=3)
 
-pass_label = Label(rpg_wid, text="PASSWORD", font=('console', 30))
+pass_label = Label(rpg_wid, text="PASSWORD",
+                   background="white", font=('console', 30))
 pass_label.grid(
     row=1, column=0, columnspan=3, pady=15)
 
@@ -63,11 +71,12 @@ def remove():
     pass_label.config(text="")
 
 
+#
 Button(rpg_wid, text='Weak', borderwidth=1,
-       width=11, height=2, activebackground='#ff0027', command=lambda: [remove(), weak()]).grid(row=3, column=0)
+       width=11, height=2, command=lambda: [remove(), weak()]).grid(row=3, column=0)
 Button(rpg_wid, text='Medium', borderwidth=1, width=11, height=2,
-       activebackground='#ffef00', command=lambda: [remove(), medium()]).grid(row=3, column=1)
+       command=lambda: [remove(), medium()]).grid(row=3, column=1)
 Button(rpg_wid, text='Strong', borderwidth=1, width=11, height=2,
-       activebackground='#a2ff00', command=lambda: [remove(), strong()]).grid(row=3, column=2)
+       command=lambda: [remove(), strong()]).grid(row=3, column=2)
 
 rpg_wid.mainloop()
