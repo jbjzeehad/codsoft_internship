51ef2bd392269f892f5c5db1fb29d5233703731c
jbjzeehad <ilamta.97@gmail.com>
2023-08-21 17:01:34 +0600

    ui done

--- CODSOFT/task_03/task_03.py
+++ CODSOFT/task_03/task_03.py
@@ -9,7 +9,7 @@ import pyperclip
 
 
 rpg_wid = Tk()
-rpg_wid.geometry('385x385')
+rpg_wid.geometry('366x370')
 rpg_wid.resizable(False, False)
 rpg_wid.title('Password Generator')
 rpg_wid.iconbitmap(r'F:\codsoft_internship\CODSOFT\task_03\rpg.ico')
@@ -27,10 +27,22 @@ length_pass = IntVar
 top_img = PhotoImage(file="F:\codsoft_internship\CODSOFT\\task_03\\rpg_bg.png")
 top_bg_img = top_img.subsample(3, 3)
 Label(rpg_wid, image=top_bg_img).grid(
-    row=0, column=0, columnspan=4)
-Button(rpg_wid, text='Weak').grid(row=3, column=1)
-Button(rpg_wid, text='Medium').grid(row=3, column=2)
-Button(rpg_wid, text='Strong').grid(row=3, column=3)
+    row=0, column=0, columnspan=3)
+
+
+Label(rpg_wid, text="PASSWORD", font=('console', 30)).grid(
+    row=1, column=0, columnspan=3, pady=20)
+
+Label(rpg_wid, text="Length", font=('verdana', 20)).grid(
+    row=2, column=0, columnspan=3, pady=21)
+
+
+Button(rpg_wid, text='Weak', borderwidth=1,
+       width=11, height=2, activebackground='#ff0027').grid(row=3, column=0)
+Button(rpg_wid, text='Medium', borderwidth=1, width=11, height=2,
+       activebackground='#ffef00').grid(row=3, column=1)
+Button(rpg_wid, text='Strong', borderwidth=1, width=11, height=2,
+       activebackground='#a2ff00').grid(row=3, column=2)
 
 
 rpg_wid.mainloop()
