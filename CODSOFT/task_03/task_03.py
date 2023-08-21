e00490bc525a2aed94b9639ad99753388cb74076
jbjzeehad <ilamta.97@gmail.com>
2023-08-21 18:29:54 +0600

    all gui and logic done

--- CODSOFT/task_03/task_03.py
+++ CODSOFT/task_03/task_03.py
@@ -15,14 +15,24 @@ rpg_wid.title('Password Generator')
 rpg_wid.iconbitmap(r'F:\codsoft_internship\CODSOFT\task_03\rpg.ico')
 
 
-weak_pass = 'abcdefghijklmnopqrstuvwxyz'
-medium_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-strong_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+def weak():
+    length = int(length_pass.get())
+    final_pass = ""
+    weak_pass = 'abcdefghijklmnopqrstuvwxyz'
+    for i in range(0, length):
+        final_pass = final_pass + random.choice(weak_pass)
+    Label(rpg_wid, text=final_pass, font=('console', 30)).grid(
+        row=1, column=0, columnspan=3, pady=20)
 
-length_pass = IntVar
+# def medium():
+#   medium_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# def strong():
+#   strong_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
 
-# def weak():
 
+length_pass = StringVar()
+length_pass_entry = Entry(
+    rpg_wid, textvariable=length_pass, font=('verdana', 20), justify=CENTER, borderwidth=0).grid(row=2, column=0, columnspan=3, pady=21)
 
 top_img = PhotoImage(file="F:\codsoft_internship\CODSOFT\\task_03\\rpg_bg.png")
 top_bg_img = top_img.subsample(3, 3)
@@ -30,15 +40,20 @@ Label(rpg_wid, image=top_bg_img).grid(
     row=0, column=0, columnspan=3)
 
 
-Label(rpg_wid, text="PASSWORD", font=('console', 30)).grid(
+lbl = Label(rpg_wid, text="PASSWORD", font=('console', 30))
+lbl.grid(
     row=1, column=0, columnspan=3, pady=20)
 
-Label(rpg_wid, text="Length", font=('verdana', 20)).grid(
-    row=2, column=0, columnspan=3, pady=21)
+
+def remove():
+    lbl.config(text="")
+# clr_pass_label = pass_label.destroy()
+
+# Label(rpg_wid, text="Length", font=('verdana', 20)).grid(row=2, column=0, columnspan=3, pady=21)
 
 
 Button(rpg_wid, text='Weak', borderwidth=1,
-       width=11, height=2, activebackground='#ff0027').grid(row=3, column=0)
+       width=11, height=2, activebackground='#ff0027', command=lambda: [remove(), weak()]).grid(row=3, column=0)
 Button(rpg_wid, text='Medium', borderwidth=1, width=11, height=2,
        activebackground='#ffef00').grid(row=3, column=1)
 Button(rpg_wid, text='Strong', borderwidth=1, width=11, height=2,
