4c0ace34c1d7437e1aa1d0909842f7ecfae141b1
jbjzeehad <ilamta.97@gmail.com>
2023-08-21 19:38:31 +0600

    done but not done

--- CODSOFT/task_03/task_03.py
+++ CODSOFT/task_03/task_03.py
@@ -21,13 +21,28 @@ def weak():
     weak_pass = 'abcdefghijklmnopqrstuvwxyz'
     for i in range(0, length):
         final_pass = final_pass + random.choice(weak_pass)
-    Label(rpg_wid, text=final_pass, font=('console', 30)).grid(
-        row=1, column=0, columnspan=3, pady=20)
+    Label(rpg_wid, text=final_pass, font=('console', 13)).grid(
+        row=1, column=0, columnspan=3, pady=15)
 
-# def medium():
-#   medium_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-# def strong():
-#   strong_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+
+def medium():
+    length = int(length_pass.get())
+    final_pass = ""
+    medium_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    for i in range(0, length):
+        final_pass = final_pass + random.choice(medium_pass)
+    Label(rpg_wid, text=final_pass, font=('console', 13)).grid(
+        row=1, column=0, columnspan=3, pady=15)
+
+
+def strong():
+    length = int(length_pass.get())
+    final_pass = ""
+    strong_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+    for i in range(0, length):
+        final_pass = final_pass + random.choice(strong_pass)
+    Label(rpg_wid, text=final_pass, font=('console', 13)).grid(
+        row=1, column=0, columnspan=3, pady=15)
 
 
 length_pass = StringVar()
@@ -39,25 +54,20 @@ top_bg_img = top_img.subsample(3, 3)
 Label(rpg_wid, image=top_bg_img).grid(
     row=0, column=0, columnspan=3)
 
-
-lbl = Label(rpg_wid, text="PASSWORD", font=('console', 30))
-lbl.grid(
-    row=1, column=0, columnspan=3, pady=20)
+pass_label = Label(rpg_wid, text="PASSWORD", font=('console', 30))
+pass_label.grid(
+    row=1, column=0, columnspan=3, pady=15)
 
 
 def remove():
-    lbl.config(text="")
-# clr_pass_label = pass_label.destroy()
-
-# Label(rpg_wid, text="Length", font=('verdana', 20)).grid(row=2, column=0, columnspan=3, pady=21)
+    pass_label.config(text="")
 
 
 Button(rpg_wid, text='Weak', borderwidth=1,
        width=11, height=2, activebackground='#ff0027', command=lambda: [remove(), weak()]).grid(row=3, column=0)
 Button(rpg_wid, text='Medium', borderwidth=1, width=11, height=2,
-       activebackground='#ffef00').grid(row=3, column=1)
+       activebackground='#ffef00', command=lambda: [remove(), medium()]).grid(row=3, column=1)
 Button(rpg_wid, text='Strong', borderwidth=1, width=11, height=2,
-       activebackground='#a2ff00').grid(row=3, column=2)
-
+       activebackground='#a2ff00', command=lambda: [remove(), strong()]).grid(row=3, column=2)
 
 rpg_wid.mainloop()
