f4d56fe58fc5082bec3a05ae56196d8637c404a1
jbjzeehad <ilamta.97@gmail.com>
2023-08-21 15:53:28 +0600

    gui done

--- CODSOFT/task_03/task_03.py
+++ CODSOFT/task_03/task_03.py
@@ -9,9 +9,10 @@ import pyperclip
 
 
 rpg_wid = Tk()
-rpg_wid.geometry('500x500')
+rpg_wid.geometry('385x385')
 rpg_wid.resizable(False, False)
 rpg_wid.title('Password Generator')
+rpg_wid.iconbitmap(r'F:\codsoft_internship\CODSOFT\task_03\rpg.ico')
 
 
 weak_pass = 'abcdefghijklmnopqrstuvwxyz'
@@ -23,6 +24,10 @@ length_pass = IntVar
 # def weak():
 
 
+top_img = PhotoImage(file="F:\codsoft_internship\CODSOFT\\task_03\\rpg_bg.png")
+top_bg_img = top_img.subsample(3, 3)
+Label(rpg_wid, image=top_bg_img).grid(
+    row=0, column=0, columnspan=4)
 Button(rpg_wid, text='Weak').grid(row=3, column=1)
 Button(rpg_wid, text='Medium').grid(row=3, column=2)
 Button(rpg_wid, text='Strong').grid(row=3, column=3)
