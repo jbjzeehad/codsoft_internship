e801871bc05c8ea394a440ec5163576c29540d80
jbjzeehad <ilamta.97@gmail.com>
2023-08-21 13:05:59 +0600

    rpg ui

--- CODSOFT/task_03/task_03.py
+++ CODSOFT/task_03/task_03.py
@@ -1,3 +1,31 @@
 
 
 # @ Jubayer Bin Jaman | Codsoft
+
+
+from tkinter import *
+import random
+import pyperclip
+
+
+rpg_wid = Tk()
+rpg_wid.geometry('500x500')
+rpg_wid.resizable(False, False)
+rpg_wid.title('Password Generator')
+
+
+weak_pass = 'abcdefghijklmnopqrstuvwxyz'
+medium_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+strong_pass = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+
+length_pass = IntVar
+
+# def weak():
+
+
+Button(rpg_wid, text='Weak').grid(row=3, column=1)
+Button(rpg_wid, text='Medium').grid(row=3, column=2)
+Button(rpg_wid, text='Strong').grid(row=3, column=3)
+
+
+rpg_wid.mainloop()
